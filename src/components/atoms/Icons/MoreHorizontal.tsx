fc2a7271518df01a60a009bc8e999ae11975043b
Julian Sepulveda <juliank1337@gmail.com>
2026-02-12 12:21:38 +0100

    feat: add MoreHorizontal component

---
+++ src/components/atoms/Icons/MoreHorizontal.tsx
@@ -0,0 +1,18 @@
+export const MoreHorizontal = ({ size = 24, color = "currentColor", ...props }: React.SVGProps<SVGSVGElement> & { size?: number, color?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <circle cx="12" cy="12" r="1" />
+    <circle cx="19" cy="12" r="1" />
+    <circle cx="5" cy="12" r="1" />
+  </svg>
+);
